51b7e5b3cdbfcb2fd5e6814dde62428fe72e2e3c
Mark N Broadhead <marknbroadhead@gmail.com>
2015-02-08 21:54:58 -0700

    built out more in server.js and renamed setup to setup.js, adding more stuff too.

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-"use strict";
+'use strict';
 
-var express = require('express'),
-var app = express();
+var express 	= require('express');
+	app 		= express(),
+	gpio 		= require('pi-gpio'),
+	config 		= require('./setup'),
+
+	Light 		= require('./Light'),
+	red 		= new Light(gpio, config.RED_POWER, config.RED_BUTTON, 'red'),
+	yellow 		= new Light(gpio, config.YELLOW_POWER, config.YELLOW_BUTTON, 'yellow'),
+	green  		= new Light(gpio, config.GREEN_POWER, config.GREEN_BUTTON, 'green'),
 
 
+	router = express.router();
 
-app.listen(3000);
+router.route('/light')
+	.get(function(req, res) {
+
+	});
+
+app.use('/api', router);
+
+app.listen(config.port);
+console.log('StopLight started on port ' + config.port);
---
+++ setup.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+var RED_POWER = 16;
+var YELLOW_POWER = 18;
+var GREEN_POWER = 15;
+
+var RED_BUTTON = 29;
+var YELLOW_BUTTON = 31;
+var GREEN_BUTTON = 33;
+
+var PORT = 3000;
+
+};
